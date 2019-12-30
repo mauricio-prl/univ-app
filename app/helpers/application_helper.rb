5e80d48ed2390bd266a03b4819c375d3f68f2313
Mauricio P <mauriciopdvg@gmail.com>
2019-12-30 16:04:59 -0300

    Add flash and errors messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@
 module ApplicationHelper
   def flash_messages
     classes = {
-      'notice' => 'success',
-      'alert' => 'negative'
+      'notice' => '#00e676 green accent-3',
+      'alert' => '#b71c1c red darken-4'
     }
 
     render 'layouts/flash_message', alert_classes: classes

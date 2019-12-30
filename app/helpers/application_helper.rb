60babbd88c05b9c7b01be676ff0d72a2dde2ac8f
Mauricio P <mauriciopdvg@gmail.com>
2019-12-30 14:37:27 -0300

    Add Form for course

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_messages
+    classes = {
+      'notice' => 'success',
+      'alert' => 'negative'
+    }
+
+    render 'layouts/flash_message', alert_classes: classes
+  end
 end

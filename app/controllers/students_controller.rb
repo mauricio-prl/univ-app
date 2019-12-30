0947715e42ca92d31d999485b3d8b49d6bcb00f2
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-12-30 14:11:23 -0300

    Add students controller (#10)

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class StudentsController < ApplicationController
+  before_action :set_student, except: %i[index new create]
+
+  def index
+    @students = Student.order(:name)
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to students_path, notice: 'Student successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @student.update(student_params)
+      redirect_to students_path, notice: 'Student successfully updated.'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    redirect_to students_path, notice: 'Student successfully deleted.' if @student.destroy
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+end

0d5eac15bfcb2ddf81a6804b94119753790a83aa
Mauricio P <mauriciopdvg@gmail.com>
2019-12-30 02:57:33 -0300

    Add Courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,20 +1,50 @@
+# frozen_string_literal: true
+
 class CoursesController < ApplicationController
+  before_action :set_course, except: %i[index new create]
+
   def index
     @courses = Course.order(:name)
   end
 
   def new
+    @course = Course.new
   end
 
   def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to root_path, notice: 'Course successfully created.'
+    else
+      render 'new'
+    end
   end
 
-  def edit
-  end
+  def show; end
+
+  def edit; end
 
   def update
+    if @course.update(course_params)
+      redirect_to root_path, notice: 'Course successfully updated.'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @course.destroy
+    redirect_to root_path, notice: 'Course successfully deleted.'
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:short_name, :name, :description)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
   end
 end
